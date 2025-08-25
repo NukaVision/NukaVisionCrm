6f3633fa83bf1f5fdd6d80b52360ffe224ff9400
MrSkyGodz <yunus.akdal@gmail.com>
2025-08-25 21:49:50 +0000

    state machine development

---
+++ nukavisioncrm/nvcrm/doctype/offer/offer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, yunus ahmet akdal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Offer", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ nukavisioncrm/nvcrm/doctype/offer/test_offer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, yunus ahmet akdal and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOffer(FrappeTestCase):
+	pass
